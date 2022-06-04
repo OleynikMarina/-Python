624f6376bb9e11456f2e08db2902bfcc02679f72
OleynikMarina <102597235+OleynikMarina@users.noreply.github.com>
2022-06-04 19:41:17 +0300

    create utils.ry

---
+++ utils.py
@@ -0,0 +1,14 @@
+from requests import get, utils
+import json
+
+URL = 'https://www.cbr-xml-daily.ru/daily_json.js'
+
+response = get(URL)
+encodings = utils.get_encoding_from_headers(response.headers)
+content = response.content.decode(encoding=encodings)
+dict_json = json.loads(response.text)
+
+def currency_rates(val):
+    if val in content:
+        print(f"Курс {val} = {dict_json['Valute'][val]['Value']}")
+    else: print('Неверный формат')
